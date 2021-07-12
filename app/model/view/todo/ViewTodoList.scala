49716aaec2307ef122d27b12612c7b822a4b10a3
kimdj2 <1222kdj@gmail.com>
2021-07-12 18:35:26 +0900

    july 12 modify todolist

---
+++ app/model/view/todo/ViewTodoList.scala
@@ -0,0 +1,23 @@
+package model.view
+
+import model.ViewValueHome
+import model.item.TodoList
+
+// TodoListのviewvalue
+case class ViewValueTodoList(
+    viewValue: ViewValueHome,
+    model: Seq[TodoList]
+)
+
+object ViewValueTodoList {
+  def apply(model: Seq[TodoList]): ViewValueTodoList = {
+    ViewValueTodoList(
+      viewValue = ViewValueHome(
+        title = "TODO LIST",
+        cssSrc = Seq("main.css", "todo/list.css"),
+        jsSrc = Seq("main.js")
+      ),
+      model = model
+    )
+  }
+}
